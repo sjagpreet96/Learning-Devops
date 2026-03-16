54b50068fb15024b85f3e478a2065395e3f368a5
jagpreet <sjagpreet878@gmail.com>
2026-03-16 22:26:22 +0000

    fixed nested

---
+++ Bash-scripting/System_information.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "===Sytem Information=="
+
+hostname=$(hostname)
+echo "Hostname: $hostname"
+
+usr=$(whoami)
+echo "User: $usr"
+
+dic=$(pwd)
+echo "Current Directory: $dic"
+
+date=$(date +%y-%m-%d)
+echo "Date: $date"
+
+disk_use=$(df -h)
+echo -e "Disk Usage \n $disk_use"
+
+memory=$(free -h)
+echo -e "Memory Usage: \n $memory"
+
+up_time=$(uptime -p)
+echo -e "Uptime: \n $up_time"
