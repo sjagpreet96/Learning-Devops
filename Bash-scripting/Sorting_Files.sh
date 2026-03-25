41534aeb03d6dc1214793bd13cf4c363e299321b
jagpreet <sjagpreet878@gmail.com>
2026-03-25 20:36:26 +0000

    comments and removed extra sh.save file

--- Bash-scripting/Sorting_Files.sh
+++ Bash-scripting/Sorting_Files.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 echo "Fetching the files"
 
-#function to check if directory exist already if not then create one
-
+#function to check if directory exist already if not then create one and move the file 
 chk_move() {
 	local dir="$1"
 	local file="$2"
@@ -14,9 +13,11 @@ chk_move() {
 	echo "$file moved successfully"	
 }
 
+#itreats all the files in current dir and check there extension 
 for file in *; do
 	if  [ -f "$file" ]; then
 		echo "Working on $file"
+		#get file name after . from end
 		file_type="${file##*.}"
 		result=$(chk_move "$file_type" "$file")
 		echo "$result"
